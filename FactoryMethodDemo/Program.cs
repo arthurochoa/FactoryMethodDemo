d8f12079db52633b38f3143bbc6ff072cacc602a
Arthur Ochoa <arthurochoa@outlook.com>
2016-10-29 14:00:18 -0700

    Add project files.

---
+++ FactoryMethodDemo/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 3; i++)
+            {
+                var position = Factory.Get(i);
+                Console.WriteLine("Where id = {0}, position = {1} ", i, position.Title);
+            }
+        }
+
+        abstract class Position
+        {
+            public abstract string Title { get; }
+        }
+
+        class Manager : Position
+        {
+            public override string Title
+            {
+                get
+                {
+                    return "Manager";
+                }
+            }
+        }
+
+        class Clerk : Position
+        {
+            public override string Title
+            {
+                get
+                {
+                    return "Clerk";
+                }
+            }
+        }
+
+        class Programmer : Position
+        {
+            public override string Title
+            {
+                get
+                {
+                    return "Programmer";
+                }
+            }
+        }
+
+        static class Factory
+        {
+            /// <summary>
+            /// Decides which class to instantiate.
+            /// </summary>
+            public static Position Get(int id)
+            {
+                switch (id)
+                {
+                    case 0:
+                        return new Manager();
+                    case 1:
+                    case 2:
+                        return new Clerk();
+                    case 3:
+                    default:
+                        return new Programmer();
+                }
+            }
+        }
+
+
+    }
+}
